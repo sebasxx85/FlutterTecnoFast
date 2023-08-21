4e8cf538e59f93fc4d110303a287507ed5e19809
unknown <ssepulveda@NB_SSEPULVEDA.tfa.local>
2023-08-21 10:37:15 -0400

    commitfromtecno

---
+++ android/app/src/main/kotlin/com/example/tecno_fast_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tecno_fast_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Scaffold].
+
+void main() {
+  var app = MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+    title: const Text("Hola mundo"),
+  )));
+
+  runApp(app);
+}
